1ff75fbef451fb7e345e96199c61ddd7bff2d88c
shibamirai <t_shiba@miraino-katachi.co.jp>
2025-09-15 09:52:55 +0900

    カテゴリ名クリックでもフィルタリング

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -2,22 +2,24 @@
 
 <article class="transition-colors duration-300 hover:bg-gray-100 border border-black/0 hover:border-opacity-5 rounded-xl">
   <div class="py-6 px-4 h-full flex flex-col">
-    <div>
-      <img src="https://miraino-katachi.co.jp/wp-content/uploads/2024/04/l_1945ba85-a2b9-4574-b9aa-3ac67970eafc.webp" alt="ブログ画像">
-    </div>
-    <h1 class="mt-2 font-bold line-clamp-1">
-      <a href="/posts/{{ $post->slug }}">
-          {{ $post->title }}
-      </a>
-    </h1>
-    <div class="mt-2 flex justify-between items-center">
-      <a href="/"
-        class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-        style="font-size: 10px"
-      >{{ $post->category->name }}</a>
+    <a href="/posts/{{ $post->slug }}">
+      <div>
+        <img src="https://miraino-katachi.co.jp/wp-content/uploads/2024/04/l_1945ba85-a2b9-4574-b9aa-3ac67970eafc.webp" alt="ブログ画像">
+      </div>
+      <h1 class="mt-2 font-bold line-clamp-1">
+        {{ $post->title }}
+      </h1>
+    </a>
 
+    <div class="mt-2 flex justify-between items-center">
+      <a href="/?category={{ $post->category->id }}"
+        class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold hover:bg-blue-300 hover:text-white"
+        style="font-size: 10px"
+          >{{ $post->category->name }}
+      </a>
+      
       <span class="block text-gray-400 text-xs">
-          <time>{{ $post->created_at->diffForHumans() }}</time>
+        <time>{{ $post->created_at->diffForHumans() }}</time>
       </span>
     </div>
   </div>
